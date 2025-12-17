64e18c3c6e232f929e7d8e04e6a459309d9d3c36
unknown <abhinashm606@gmail.com>
2025-12-18 00:30:05 +0530

    first commit

---
+++ asssigmeent 2/ABHI.js
@@ -0,0 +1,54 @@
+ function abhi(){
+    let age=prompt("enter your age")
+
+
+if(age>=18){
+    alert("vote")
+}
+else{
+    alert("not")
+}
+
+let number=prompt("enter a number")
+
+if(number>=0){
+    alert("positive")
+}
+else if(number<0){
+    alert("negative")
+}
+
+else{
+    alert("zero")
+}
+
+let marks=prompt("enter a marks")
+
+if(marks>=80 && marks <=100){
+    alert("grade A ")
+}
+else if(marks>=60 && marks <=80)
+{
+    alert("grade B")
+}
+else if(marks>=40 && marks <=60){
+    alert("grade C")
+}
+else{
+    alert("grade F")
+}
+
+let check=prompt("enter your password")
+password=58934;
+
+if(password==check){
+    alert("aaccess granted")
+}
+else{
+    alert("denied")
+}
+
+
+
+}
+abhi()
---
+++ asssigmeent 2/assigment 3/AM.js
@@ -0,0 +1,39 @@
+//question number 2.
+function avinash(){
+    let firstname=prompt("Enter a first name")
+    let lastname=prompt("enter a last name")
+
+    let fullname=(firstname.concat(lastname))
+
+    let fn=firstname+lastname
+
+    console.log("hello" + fn)
+
+    alert("hello" + fn)
+
+
+//question no 1.
+    let word=prompt("enter a word")
+    alert("the word is" +word.length);
+
+    // qustion no 5
+let sentence=prompt("enter a sentence")
+
+letword=prompt("enter a word")
+alert(sentence.split())
+
+
+letword=[1,3,"kya","thor"]
+console.log(word.reverse());
+alert(word.reverse())
+
+//question 4.
+
+let list=prompt(" apple,banana,cheery")
+let array=list.split()
+
+let seconditem=array[1]
+
+alert("the second itam is"+ seconditem )
+ }
+avinash()
